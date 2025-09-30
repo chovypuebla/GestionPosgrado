94ee68918b8169916153f51a4a7b3ecc811c2900
Rodolfo Romero Mirón <136407498+chovypuebla@users.noreply.github.com>
2025-09-30 12:07:14 -0600

    Actualización

---
+++ Base de Datos/dbCambiosGestionFCC.sql
@@ -0,0 +1,17 @@
+Insert into coordinador (id_coordinador, usuario, contrasenia, rol)
+values ('1', '201921492', 'rodorm24', 'coordinador');
+
+Insert into alumno (matricula, nombre)
+values ('201649351', 'Roberto Medina Mendoza');
+
+Insert into curso (id_curso, nombre, periodo, anio, fechaRegistroCurso)
+values ('1', 'Enseñanza de Matemáticas y Computación a Licenciatura', 'Primavera', '2026', '2025-09-30 12:00:00' );
+
+
+alter table curso
+alter column nombre varchar(255);
+
+Select * from curso;
+
+-- Modificaciones a la columna del nombre del curso
+-- añadir en la db alumno, coordinador (indispensable para inicio de sesión)
